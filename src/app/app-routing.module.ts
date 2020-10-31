b93591dec1f697f44cf27547e3917f46059b1e9b
nika2212 <nikabarsukovi@gmail.com>
2020-11-01 01:54:08 +0400

    Late Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthenticationGuard } from './core/guards/authentication.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'main', pathMatch: 'full' },
+  { path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) },
+  { path: 'main', loadChildren: () => import('./modules/main/main.module').then(m => m.MainModule), canActivate: [AuthenticationGuard] },
+
+  { path: '**', redirectTo: 'main' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthService } from './core/services/auth.service';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { AuthenticationInterceptor } from './core/interceptors/authentication.interceptor';
+import { HttpErrorInterceptor } from './core/interceptors/http-error.interceptor';
+import { AccessTokenService } from './core/services/access-token.service';
+import { GrowlModule } from './shared/components/growl/growl.module';
+import { GrowlService } from './core/services/growl.service';
+import { LoaderModule } from './shared/components/loader/loader.module';
+import { LoaderService } from './core/services/loader.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    GrowlModule,
+    LoaderModule
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    AccessTokenService,
+    GrowlService,
+    LoaderService,
+
+    { provide: HTTP_INTERCEPTORS, useClass: AuthenticationInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
